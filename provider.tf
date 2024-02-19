45a6b54310f7158f137c4a745163364327f58622
root <root@ip-172-31-44-122.ap-south-1.compute.internal>
2024-02-19 14:18:28 +0000

    all

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.37.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform6055"
+    key    = "terraform333"
+    region = "ap-south-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "ap-south-1"
+}
+
---
+++ variables.tf
@@ -0,0 +1,23 @@
+ variable "zone_name" {
+
+}
+
+
+variable "hosted_zone_id" {
+
+}
+
+variable "app_alb_record_name" {
+
+}
+
+
+
+variable "app_alb_dns_name" {
+
+}
+
+variable "app_alb_zone_id" {
+
+}
+
